60de76a8e153f1aab287fcdf81d4f69f13d22938
Mohit Madaan <mmadaan171@gmail.com>
2024-02-08 06:40:34 +0530

    yupppp

--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -1,25 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
+import { useDispatch, useSelector } from "react-redux";
 import {
   CALCULATE_SUBTOTAL,
   CALCULATE_TOTAL_QUANTITY,
   selectCartItems,
   selectCartTotalAmount,
 } from "../../redux/slice/cartSlice";
-import { useDispatch, useSelector } from "react-redux";
-import { selectEmail } from "../../redux/slice/authReducer";
+import { selectEmail } from "../../redux/slice/authSlice";
 import {
   selectBillingAddress,
   selectShippingAddress,
-} from "../../redux/slice/checkoutReducer";
+} from "../../redux/slice/checkoutSlice";
 import { toast } from "react-toastify";
 import CheckoutForm from "../../components/checkoutForm/CheckoutForm";
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
 
 const Checkout = () => {
-  const [message, setMessage] = useState("Initialising Checkout...");
+  const [message, setMessage] = useState("Initializing checkout...");
   const [clientSecret, setClientSecret] = useState("");
 
   const cartItems = useSelector(selectCartItems);
@@ -30,20 +30,19 @@ const Checkout = () => {
   const billingAddress = useSelector(selectBillingAddress);
 
   const dispatch = useDispatch();
-
   useEffect(() => {
     dispatch(CALCULATE_SUBTOTAL());
     dispatch(CALCULATE_TOTAL_QUANTITY());
   }, [dispatch, cartItems]);
 
-  const description = `mmShop payment: email : ${customerEmail}, Amount: ${totalAmount}`;
+  const description = `eShop payment: email: ${customerEmail}, Amount: ${totalAmount}`;
 
   useEffect(() => {
-
-    // http://localhost:4242/create-payment-intent
-
+    // http://localhost:4242/create-payment-intent..
+    
     // Create PaymentIntent as soon as the page loads
-    fetch("https://mmshop.onrender.com/create-payment-intent", {
+    fetch("http://localhost:4242/create-payment-intent", {
+
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -58,7 +57,6 @@ const Checkout = () => {
         if (res.ok) {
           return res.json();
         }
-
         return res.json().then((json) => Promise.reject(json));
       })
       .then((data) => {
